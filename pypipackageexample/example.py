b347396e52f124873bf4bbff207f353ea8995877
Michael Shekasta <shkasta@post.bgu.ac.il>
2020-08-03 17:21:40 +0300

    adding setup tools and requirements file;

--- pypipackageexample/example.py
+++ pypipackageexample/example.py
@@ -1,5 +1,12 @@
 import math
+from typing import Tuple, Any
+import numpy as np
+from scipy.stats import pearsonr
 
 
 def abs_sin(x: float = 0) -> float:
     return math.sin(abs(x))
+
+
+def abs_pearson(x: list, y: list) -> Tuple[float, Any]:
+    return pearsonr(np.absolute(x), np.absolute(y))
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,6 +1,10 @@
-from src.example import abs_sin
-
+from pypipackageexample.example import abs_sin, abs_pearson
+import numpy as np
 
 def test_abs_sin():
     assert abs_sin(0) == 0.0
     assert abs_sin(1) == abs_sin(-1)
+
+
+def test_abs_pearson():
+    assert abs_pearson(np.asarray([1, 2]), np.asarray([-1, -2])) == (1.0, 1.0)
